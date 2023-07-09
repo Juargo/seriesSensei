47e718fc1d2a5cebf1b64e8c7fb54ba7549f5190
Jorge R <jorge.retamal@zweicom.com>
2023-07-08 22:58:28 -0400

    feat[getall series]: Se implementa getall series y se conecta a mongodb

--- index.py
+++ index.py
@@ -1,64 +1,25 @@
 """Module principal """
 
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["seriesSensei"]
+collection = db["series"]
 
 
 @app.route("/series/getall", methods=["GET"])
 def get_all_series():
     """Function return all series"""
-    return {
-        "07-Ghost": {
-            "sinopsis": (
-                "Un joven llamado Teito es el único superviviente de su país,"
-                " el cual fue destruido por el Imperio de Barsburg."
-                " Ahora, Teito se encuentra en la academia militar de Barsburg,"
-                " donde entabla amistad con otros estudiantes y"
-                " descubre que tiene poderes sobrenaturales."
-                " Así comienza su búsqueda de venganza contra el Imperio y"
-                " su lucha por descubrir su verdadero destino."
-            ),
-            "genres": {
-                "Action": "50%",
-                "Adventure": "80%",
-                "Drama": "90%",
-                "Mystery": "60%",
-                "Supernatural": "100%",
-                "Fantasy": "100%",
-                "Romance": "40%",
-                "Slice of life": "20%",
-                "Ecchi": "0%",
-                "Erotica": "0%",
-                "Hentai": "0%",
-                "Sci-Fi": "0%",
-            },
-        },
-        "Berserk": {
-            "sinopsis": (
-                "La historia se centra en Guts, un guerrero solitario cuya"
-                " única misión en la vida es matar demonios llamados Apóstoles,"
-                " los cuales son seres inhumanos que se alimentan de humanos."
-                " Guts es un poderoso espadachín conocido como el Espadachín Negro,"
-                " y su espada maldita le otorga una fuerza formidable."
-                " A lo largo de la historia, Guts se enfrenta a múltiples enemigos y"
-                " se ve envuelto en una lucha épica por la supervivencia y la redención."
-            ),
-            "genres": {
-                "Action": "100%",
-                "Adventure": "90%",
-                "Horror": "80%",
-                "Drama": "100%",
-                "Supernatural": "70%",
-                "Fantasy": "100%",
-                "Romance": "10%",
-                "Suspense": "60%",
-                "Gourmet": "0%",
-                "Slice of life": "10%",
-                "Ecchi": "0%",
-                "Erotica": "0%",
-                "Hentai": "0%",
-                "Sci-Fi": "0%",
-            },
-        },
-    }
+    series = collection.find()
+    response = {}
+    for series_object in series:
+        for serie in series_object.keys():
+            if serie != "_id":
+                response[serie] = {
+                    "sinopsis": series_object[serie]["sinopsis"],
+                    "genres": series_object[serie]["genres"],
+                }
+    return response
