427b99c4d4e1b02034053b9acb64081bbc2af87c
Jorge R <jorge.retamal@zweicom.com>
2023-07-09 01:47:12 -0400

    cors

--- index.py
+++ index.py
@@ -2,8 +2,10 @@
 
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["seriesSensei"]
